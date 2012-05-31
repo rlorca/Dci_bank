0bbcb1dfe855158daf823c8b633c8473b76d6f6d
Rodrigo <lorchaos@gmail.com>
2012-05-31 23:26:39 +0200

    Added unit test for context

--- lib/account.rb
+++ lib/account.rb
@@ -37,26 +37,11 @@ end
 class TransferContext
 
   def self.call(src, target, amount)
-     TransferContext.new(src, target).call(amount)
-  end
- 
-  def initialize(src, target)
-    @src = src.extend Transfer::SourceAccount
-    @target = target.extend Transfer::TargetAccount          
-  end
-
-  def call(amount)
-    @src.withdraw amount
-    @target.deposit amount
+    
+    src.extend Transfer::SourceAccount
+    target.extend Transfer::TargetAccount          
+    
+    src.withdraw amount
+    target.deposit amount
   end
 end
-
-
-#what shouuld go in a controller
-src = Account.new 100
-target = Account.new 100
-
-TransferContext.call(src, target, 40)
-
-puts "Source balance #{src.balance}"
-puts "Target balance #{target.balance}"
--- test/test_account.rb
+++ test/test_account.rb
@@ -1,17 +1,26 @@
 require 'test/unit'
 require 'shoulda'
 
+require 'account'
 
 class TestAccount < Test::Unit::TestCase
   
-  context "simple account operations" do
+  context "Transfer" do 
 
-    should "increase balance" do
+    should "money should be transferred from source to target" do 
+      
+      src = Account.new 100
+      target = Account.new 100
+      
+      TransferContext.call(src, target, 40)
 
-      assert_equal 2, 2
-
+      assert_equal 60, src.balance
+      assert_equal 140, target.balance
+      
+      # this is available outside the context, not exactly what I wanted
+      #assert_equal false, src.respond_to?(:withdraw)
     end
-
+  
   end
 
 end
