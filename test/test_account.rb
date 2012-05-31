24cc7e1bf4cb333da366ab7acae8a224f67787e3
Rodrigo <lorchaos@gmail.com>
2012-05-31 22:19:55 +0200

    Added unit test folder

---
+++ test/test_account.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'shoulda'
+
+
+class TestAccount < Test::Unit::TestCase
+  
+  context "simple account operations" do
+
+    should "increase balance" do
+
+      assert_equal 2, 2
+
+    end
+
+  end
+
+end
